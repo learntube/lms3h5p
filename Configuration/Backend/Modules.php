94c45f9c5a6cfff815eebd7811a810619e167ae8
Sagar Desai <sagardesai1990@gmail.com>
2023-05-29 11:39:28 +0200

    [BUGFIX] Module icon

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -7,11 +7,12 @@ use LMS3\Lms3h5p\Controller\{
 };
 
 return [
-    'web_lms3h5p' => [
+    'lms3h5p' => [
         'parent' => 'web',
         'access' => 'user',
+        'workspaces' => 'live',
         'path' => '/module/web/lms3h5p',
-//        'iconIdentifier' => 'module-lms3h5p',
+        'icon' => 'EXT:lms3h5p/Resources/Public/Icons/Module.svg',
         'labels' => 'LLL:EXT:lms3h5p/Resources/Private/Language/locallang_mod.xlf',
         'extensionName' => 'lms3h5p',
         'controllerActions' => [
